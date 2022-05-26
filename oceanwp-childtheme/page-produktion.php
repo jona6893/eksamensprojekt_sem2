183c6a83727005fb4189f2e019b8721e8304627a
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-05-26 13:39:09 +0200

    produktion page

---
+++ oceanwp-childtheme/page-produktion.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package OceanWP WordPress theme
+ */
+get_header(); ?>
+
+<style>
+
+
+
+	</style>
+
+  <section><h1>HELLO</h1></section>
+
+
+<script>
+
+	
+
+</script>
+
+<?php get_footer(); ?>
