22bda8a2440037a9ce36ea6382f719e6ee19e818
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-05-30 16:12:35 +0200

    oidfjg

--- oceanwp-childtheme/page-vores-historie.php
+++ oceanwp-childtheme/page-vores-historie.php
@@ -16,6 +16,8 @@ get_header(); ?>
     #section1{
         display: grid;
         justify-items: center;
+        padding: 100px 2%;
+    
     }
 /* ---------- SECTION2 ---------- */
     #section2{
@@ -24,6 +26,12 @@ get_header(); ?>
         gap: 50px;
         padding: 0% 2%;
         align-items: center;
+        justify-items: center;
+    }
+    #section2 p{
+        box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
+        padding: 3%;
+        max-width: 48ch;
     }
     .foto{
         background-image: url("https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/uploads/2022/05/omos-scaled.webp");
@@ -32,6 +40,18 @@ get_header(); ?>
         width: 100%;
         height:100%;
         aspect-ratio: 1;
+    }
+
+    #section3{
+            display: grid;
+    justify-items: center;
+    padding: 100px 0%;
+    }
+
+    #section4{
+        display: grid;
+    justify-items: center;
+    padding: 100px 0%;
     }
 /* ---------- SECTION3 ---------- */
 
@@ -54,8 +74,11 @@ get_header(); ?>
     <div class="foto"></div>
 </section>
 <section id="section3">
-    <h2></h2>
-    <video src=""></video>
+  <img src="https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/themes/oceanwp-childtheme/svg/seti.webp" alt="">
+</section>
+<section id="section4">
+    <h2>Mød Founders</h2>
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/671871303?h=a7c592fc40" width="640" height="360" frameborder="0" allowfullscreen></iframe>
 </section>
 </main>
 
