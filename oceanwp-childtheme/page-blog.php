e4e22968fca68be4047f91567313c47a494c39fe
mariegoltermann <mari09a7@stud.kea.dk>
2022-06-01 14:19:27 +0200

    blogdone

--- oceanwp-childtheme/page-blog.php
+++ oceanwp-childtheme/page-blog.php
@@ -17,6 +17,139 @@ get_header(); ?>
     background-image: url("https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/uploads/2022/05/streger2.png");
     background-size: cover;
   }
+/*-------------------MOBIL---------------------*/
+#first_section{
+        padding: 5%;
+      }
+  
+      .laeseom {
+        padding-right: 15%; 
+      }
+      
+      .laeseom-container {
+        padding-bottom: 5%;
+      }
+
+      .kategorier-felter{
+        display: grid;
+        gap: 12px;
+      }
+      input{
+        padding:2%;
+        border: none;
+        border-bottom: solid darkgrey 1px;
+      }
+      .image-container {
+    display: grid;
+    width: 117%;
+     gap: 5px;
+      }
+
+
+    .grid1{
+      grid-column: 1/2;
+      grid-row: 1/2;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid2{
+      grid-column: 2/3;
+      grid-row: 1/2;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid3{
+      grid-column: 3/4;
+      grid-row: 1/2;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid4{
+      grid-column: 1/2;
+      grid-row: 2/3;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid5{
+      grid-column: 2/3;
+      grid-row: 2/3;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid6{
+      grid-column: 3/4;
+      grid-row: 2/3;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid7{
+      grid-column: 1/2;
+      grid-row: 3/4;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid8{
+      grid-column: 2/3;
+      grid-row: 3/4;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+    .grid9{
+      grid-column: 3/4;
+      grid-row: 3/4;
+      box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+    }
+/*------------- anden, tredje og fjerde sektion ------------*/
+#second_section, #third_section, #fourth_section  {
+  display: grid;
+        width: 100%;
+        height: 100%;
+        justify-items: center;
+        gap: 20px;
+        padding-right: 15%;
+        padding-left: 15%;
+        padding-bottom: 4%;
+        padding-top: 4%;
+}
+#blogbillede{
+  width: 100%;
+  padding-bottom: 5%; 
+}
+
+
+/*------------- overordnet styling af knapper, lilletekst og container med shadow sektion ------------*/
+.blogpost-container {
+        background-color: white;
+        box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+        padding: 8% 8%;
+      }
+.knapper {
+        background-color: black;
+        color: white;
+        border: none;
+        width: 150px;
+        height: 40px;
+      }
+.lilletekstogknap{
+  padding-top: 5%;
+  padding-bottom: 5%;
+}
+
+.lilletekst{
+    font-size: clamp(0rem, 0.5rem + 0.25vw, 1.25rem);
+}
+#sort_bar1{
+		background-color:black;
+    height: 203px;
+		display: grid;
+		align-items: center;
+    justify-items: center;
+
+	}
+	#sort_bar1 h1{
+    font-family: "adrianna", sans-serif;
+		font-size: 5rem;
+    font-weight: 800;
+  font-style: normal;
+		color:white;
+    font-size: clamp(3rem, 2.6rem + 2vw, 5rem);
+    text-align: center;
+	}
+
+/*-------------------DESKTOP---------------------*/
+  @media (min-width: 800px){
 /*------------- første sektion ------------*/
    #first_section{
         display: grid;
@@ -131,6 +264,12 @@ get_header(); ?>
         padding-bottom: 4%;
         padding-top: 4%;
 }
+.grid{
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: 1fr;
+}
+
 
 /*------------- tredje sektion ------------*/
 #third_section {
@@ -155,7 +294,7 @@ get_header(); ?>
         padding-bottom: 4%;
 }
 #blogbillede{
-  width: 63%;
+  width: 100%;
   padding-bottom: 5%; 
 }
 
@@ -186,6 +325,12 @@ get_header(); ?>
     flex-direction: row;
     width: 100%;
 }
+.tekstogknap{
+  display: grid;
+    width: 100%;
+    align-content: space-evenly;
+    padding-left: 5%;
+}
 .lilletekst{
   font-family: "Helvetica", sans-serif !important;
     font-weight: normal;
@@ -207,7 +352,7 @@ get_header(); ?>
   font-style: normal;
 		color:white;
 	}
-  
+}
   
 
 
@@ -252,6 +397,9 @@ get_header(); ?>
     <h1>Blog indlæg</h1>
     <div class="blogpost1">
     <div class="blogpost-container">
+      <div class="grid">
+      <img id="blogbillede" src="https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/themes/oceanwp-childtheme/svg/2021_EACE_WEB_032.png?_t=1654080646" alt="blogbillede1">
+      <div class="tekstogknap">
       <h2>ALT OM ENERGY BOOST</h2>
      <p>Energy Boost var det andet produkt Eace Gum lancerede. Det kom ud i december 2021, lidt over et år efter Vitamin Boost landede i butikkerne. Det er på rekordtid blevet et meget populært tyggegummi og ligger i butikkerne landet over. Her på siden kan du læse alt der er værd at vide om Energy Boost!  Idéen til Energy Boost…</p>
      <div class="lilletekstogknap">
@@ -260,6 +408,8 @@ get_header(); ?>
         <button class="knapper knap1">LÆS MERE</button>
         </div>
         </div>
+        </div>
+     </div>
       </div>
     </div>
   </section>
@@ -267,8 +417,11 @@ get_header(); ?>
   <section id="third_section">
     <div class="blogpost2">
     <div class="blogpost-container">
-      <h2>Eace Sommerfest</h2>
-     <p>Eace Gum inviterer til årets første og fedeste sommerfest! Vi skyder sommeren igang, fejrer at vi igen kan mødes under normale omstændigheder og byder på en aften med live musik, events, festlig stemning under åben himmel, mad og kolde drinks samt dj til langt ud på natten.</p>
+      <div class="grid">
+      <img id="blogbillede" src="https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/themes/oceanwp-childtheme/svg/2021_EACE_WEB_051.png?_t=1654080646" alt="blogbillede2">
+     <div class="tekstogknap">
+     <h2>Eace Sommerfest</h2>
+      <p>Eace Gum inviterer til årets første og fedeste sommerfest! Vi skyder sommeren igang, fejrer at vi igen kan mødes under normale omstændigheder og byder på en aften med live musik, events, festlig stemning under åben himmel, mad og kolde drinks samt dj til langt ud på natten.</p>
      <div class="lilletekstogknap">
      <p class="lilletekst">Shopify API <br> maj 04, 2022 <br> 0 kommentarer</p>
     <div class="knapper">
@@ -277,13 +430,17 @@ get_header(); ?>
         </div>
       </div>
     </div>
+  </div>
+  </div>
   </section>
 
   <section id="fourth_section">
     <div class="blogpost3">
     <div class="blogpost-container">
+      <div class="grid">
+      <img id="blogbillede" src="https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/themes/oceanwp-childtheme/svg/2021_EACE_WEB_046.png?_t=1654080646" alt="blogbillede3">
+      <div class="tekstog knap">
       <h2>NEXT GENERATION MERCH</h2>
-      <img id="blogbillede" src="https://victor-ly.dk/kea/10_eksamensprojekt/eacegum/wp-content/themes/oceanwp-childtheme/svg/blogbillede.webp?_t=1653902197" alt="blogbillede">
       <p>Vi er next generation chewing gum. Nu har vi lavet next generation merch. I samarbejde med Neutral har vi lavet en bæredygtigt merch kollektion i høj kvalitet. Læs meget mere om vores merch kollektion i dette blogindlæg.</p>
      <div class="lilletekstogknap">
      <p class="lilletekst">Shopify API <br> december 17, 2021<br> 0 kommentarer</p>
@@ -292,6 +449,8 @@ get_header(); ?>
         </div>
         </div>
       </div>
+    </div>
+    </div>
     </div>
   </section>
 </main>
